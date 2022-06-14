555b126f076b9c0a98ad821943a4e86366ac52cc
Langman <josh.langman@vcontractor.co.za>
2022-06-14 16:03:21 +0200

    Commiting enpoints for Rocket company and country as well as Launch name as well as a small clean up of error messages

--- Repository/IRocketRepository.cs
+++ Repository/IRocketRepository.cs
@@ -6,6 +6,8 @@ namespace SpaceLaunchAPI.Repository
     {
         public Task<IEnumerable<Rocket>> GetAllAsync();
         public Task<Rocket?> GetByIdAsync(string id);
+        public Task<IEnumerable<Rocket?>> GetByCountryAsync(string country);
+        public Task<IEnumerable<Rocket?>> GetByCompanyAsync(string company); 
         public Task<Rocket> AddAsync(Rocket rocket);
         public Task<Rocket?> DeleteAsync(string id);
         public Task<Rocket?> UpdateAsync(string id, Rocket rocket);
